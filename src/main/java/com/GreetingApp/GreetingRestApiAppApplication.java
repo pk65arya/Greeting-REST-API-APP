4d363995c31b3ab23ec2f6dab5d9a9a780764df2
pk65arya <pk65arya@gmail.com>
2023-08-20 13:01:17 +0530

    Using GreetingController return JSON for different HTTP Methods.

---
+++ src/main/java/com/GreetingApp/GreetingRestApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.GreetingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingRestApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingRestApiAppApplication.class, args);
+	}
+
+}
